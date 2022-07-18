26e7bdb16d10f1ead8e50f3b10b2ff8fa81bb2af
YounisWebs <107230868+YounisWebs@users.noreply.github.com>
2022-07-18 05:39:24 +0500

    Update end.js

--- end.js
+++ end.js
@@ -22,6 +22,6 @@ saveHighScore = e => {
     highScores.sort((a, b) => b.score - a.score);
     highScores.splice(5)
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign('/index.html')
+    window.location.assign('youniswebs.github.io/quize-app/')
     e.preventDefault()
 }
